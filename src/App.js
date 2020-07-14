cb030945a9edbfb2d3e1e1490847c8b8ab2b7167
Lawrence <Caulker>
2020-07-14 16:59:34 -0400

    completed sign-in component with custom button component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react"
-import "./App.css"
-
 import { Switch, Route } from "react-router-dom"
+import "./App.css"
 
 import HomePage from "./pages/homepage/homepage.component"
 import ShopPage from "./pages/shop/shop.component"
 import Header from "./components/header/header.component"
+import SignInSignUp from "./pages/signin-and-signup/signin-and-signup"
 
 function App() {
   return (
@@ -14,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInSignUp} />
       </Switch>
     </div>
   )
---
+++ src/pages/signin-and-signup/signin-and-signup.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import SignIn from "../../components/sign-in/signin.component"
+
+import "./signin-and-signup.scss"
+
+const SignInSignUp = () => {
+  return (
+    <div className="sign-in-and-sign-up">
+      <SignIn />
+    </div>
+  )
+}
+
+export default SignInSignUp
